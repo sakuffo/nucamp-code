0236c0de4cb497a9cfcf44213185b06c3dff88de
Stephen Akuffo <sa.akuffo@gmail.com>
2022-01-23 03:43:08 -0500

    modules cc

---
+++ Python/1-Fundamentals/code-challange/convert_input.py
@@ -0,0 +1,4 @@
+total_cash = 0
+user_input = input("Enter an amoumt to transfer: ")
+total_cash = total_cash + float(user_input)
+print("Total cash: " + str(total_cash))
--- Python/1-Fundamentals/week2-cc/cc_staircase_loop.py
+++
@@ -1,5 +0,0 @@
-stars = ""
-for i in range(1, 5, 2):
-    for j in range(0, 2, 1):
-        stars += "i"
-        print(stars)
