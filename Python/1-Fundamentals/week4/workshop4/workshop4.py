c3bd2dec95924187fc5348639de36fcca1dd9a3e
Stephen Akuffo <sa.akuffo@gmail.com>
2022-01-29 12:38:26 -0500

    final - no bonus

--- Python/1-Fundamentals/week4/workshop4/workshop4.py
+++ Python/1-Fundamentals/week4/workshop4/workshop4.py
@@ -10,7 +10,7 @@ from bankUser import BankUser
 """ Driver Code for Task 2 """
 # Task 2 can be found in /user.py
 
-# tom = BankUser("Tom", "4321", "password")
+# tom = User("Tom", "4321", "password")
 # print(f"{tom.name} {tom.pin} {tom.password}")
 # tom.change_name("Tommy")
 # tom.change_pin("54321")
@@ -18,10 +18,14 @@ from bankUser import BankUser
 # print(f"{tom.name} {tom.pin} {tom.password}")
 
 """ Driver Code for Task 3 """
+# Task 3 can be found in /bankUser.py'
+
 # esi = BankUser("Esi", "1234", "password")
 # print(f"{esi.name} {esi.pin} {esi.password} {esi.balance}")
 
 """ Driver Code for Task 4 """
+# Task 4 can be found in /bankUser.py
+
 # akosua = BankUser("akosua", "1234", "password")
 # akosua.show_balance()
 # akosua.deposit(1000)
@@ -30,6 +34,7 @@ from bankUser import BankUser
 # akosua.show_balance()
 
 """ Driver Code for Task 5 """
+# Task 5 can be found in /bankUser.py
 
 # create both users and initialize their balances
 kofi = BankUser("Kofi", "1234", "kpass")
