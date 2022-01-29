e8ef6db7c4377811adf80fe0c482ecaea5df8890
Stephen Akuffo <sa.akuffo@gmail.com>
2022-01-29 11:17:55 -0500

    class skeleton

--- Python/1-Fundamentals/week4/workshop4/workshop4.py
+++ Python/1-Fundamentals/week4/workshop4/workshop4.py
@@ -1,7 +1,11 @@
+# Task 1: Create a class called User.
+
 class User:
+
     def __init__(self, name, pin, password):
         pass
-      
+
+    # Task 2: Create User Class instance methods
     def change_name():
         pass
 
@@ -9,4 +13,28 @@ class User:
         pass
 
     def change_password():
+        pass
+
+# Task 3: Create BankUser subclass
+
+
+class BankUser(User):
+    def __init__(self, name, pin, password):
+        super().__init__(name, pin, password)
+
+    # Task 4: Create BankUser instance methods
+    def show_balance():
+        pass
+
+    def withdraw():
+        pass
+
+    def deposit():
+        pass
+
+    # Task 5: Transfer and request money
+    def transfer_money():
+        pass
+
+    def request_money():
         pass
